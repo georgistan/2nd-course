79ff1fa258d576a21e955879593e83e80d39dee8
Georgi Stanachkov <georgistan@abv.bg>
2024-12-08 00:59:55 +0200

    add setup for MJT lab08

---
+++ MJT/lab08/src/bg/sofia/uni/fmi/mjt/frauddetector/transaction/Transaction.java
@@ -0,0 +1,16 @@
+package bg.sofia.uni.fmi.mjt.frauddetector.transaction;
+
+import java.time.LocalDateTime;
+
+public record Transaction(
+    String transactionID,
+    String accountID,
+    double transactionAmount,
+    LocalDateTime transactionDate,
+    String location,
+    Channel channel
+) {
+    public static Transaction of(String line) {
+        return null;
+    }
+}
